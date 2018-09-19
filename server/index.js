280670d65be2f669497e622ab4fd626e67a56bb0
Benjamin Alldredge <benjaminalldredge@Benjamins-MacBook-Pro.local>
2018-09-19 17:05:59 -0600

    edit port

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 
 const app = express();
+const port = 3011;
 
 app.use(express.static(__dirname) );
+
+app.listen(port, () => console.log(`listening on port ${port}!`));
