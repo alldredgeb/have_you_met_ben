088496c407da38ba8156e310850fd83007d54091
Benjamin Alldredge <benjaminalldredge@Benjamins-MacBook-Pro.local>
2018-09-19 17:28:32 -0600

    done

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const port = 3011;
 
-app.use(express.static(__dirname + '/build') );
+console.log(__dirname + '/../build')
+app.use(express.static(__dirname + '/../build') );
 
 app.listen(port, () => console.log(`listening on port ${port}!`));
