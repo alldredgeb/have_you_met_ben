c6c604b82c2adb0ec6fe6424553fbe551d4a6fef
Benjamin Alldredge <benjaminalldredge@Benjamins-MacBook-Pro.local>
2018-09-19 17:11:58 -0600

    another pass

--- server/index.js
+++ server/index.js
@@ -3,6 +3,6 @@ const express = require('express');
 const app = express();
 const port = 3011;
 
-app.use(express.static(__dirname) );
+app.use(express.static(__dirname + '/build') );
 
 app.listen(port, () => console.log(`listening on port ${port}!`));
